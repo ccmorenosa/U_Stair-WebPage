d5038e65477244f5fd465964b163fed1f25a84ee
afmorenosa <afmorenosa@unal.edu.co>
2021-01-20 11:55:54 -0500

    Fix a download page bug

--- descargar/js/platform.js
+++ descargar/js/platform.js
@@ -6,14 +6,13 @@ if ( window.navigator.platform.match("Linux") ) {
 
 } else if (window.navigator.platform.match("Win")) {
 
-  console.log(window.navigator.platform.match("Win"));
-  console.log(window.navigator.platform.match("Linux"));
-  console.log(window.navigator.platform);
   document.getElementById("Windows-A").classList.remove("d-none");
   document.getElementById("Windows-B").classList.add("d-none");
 
 } else {
 
+  document.getElementById("Linux-A").classList.remove("d-none");
+  document.getElementById("Windows-A").classList.remove("d-none");
   document.getElementById("second-download").classList.add("d-none");
 
 }
